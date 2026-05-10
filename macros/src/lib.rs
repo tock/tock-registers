7f6f3db1256dfbc8c7ed173b27388750d2f09973
Johnathan Van Why <jrvanwhy@betterbytes.org>
2026-05-09 21:59:57 -0700

    Add a document describing register slice implementation details, in case we decide to implement that in the future.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,25 +3,26 @@
 // Copyright Tock Contributors 2026.
 // Copyright Better Bytes 2026.
 
-// TODO: Implement a RegisterArray iterator.
+// TODO: Add READMEs for every crate.
 // TODO: Implement a "clippy test" -> a crate that uses tock-registers with as many lints as
 //       possible enabled (to verify we don't trip any of them).
+// TODO: Implement UnimplementedRegister, add to operation documentation.
+// TODO: Implement a arm64_secure_vm feature (see the TODO in src/mmio.rs).
+// TODO: Implement a RegisterArray iterator.
+// TODO: Implement macro that automatically provides the type defs for Interface trait impls.
+// TODO: Investigate adding typestates into the API
+//       (https://github.com/jrvanwhy/tock-registers/pull/6).
+//       Can we use the lifetime pattern that GhostCell uses to tie state types to the particular
+//       register handle? (briefly mentioned in the presentation on 2026-04-29)
 // TODO: Improve parse error handling. There might be three classes of errors:
 //       1. Errors which immediately terminate parsing (e.g. unexpected token)
 //       2. Errors which prevent generating code, but allow parsing to continue (e.g. register
 //          reference must be to a module)
 //       3. Errors where we can still generate code (e.g. multiple #[aliased] attributes)
 // TODO: Verify that offset/size lists match the number of buses.
-// TODO: Implement UnimplementedRegister, add to operation documentation.
-// TODO: Implement macro that automatically provides the type defs for Interface trait impls.
-// TODO: Implement a arm64_secure_vm feature (see the TODO in src/mmio.rs).
+// TODO: Re-evaluate which `syn` features we need (is full necessary?).
 // TODO: Update the top-level crate doc comment. It should probably match or be similar to
 //       the README.
-// TODO: Investigate adding typestates into the API
-//       (https://github.com/jrvanwhy/tock-registers/pull/6).
-//       Can we use the lifetime pattern that GhostCell uses to tie state types to the particular
-//       register handle? (briefly mentioned in the presentation on 2026-04-29)
-// TODO: Re-evaluate which `syn` features we need (is full necessary?).
 
 // Questions to ask the group:
 // TODO: Do we want to remove the trailing commas after declarations? Easy to do, but a bit harder
