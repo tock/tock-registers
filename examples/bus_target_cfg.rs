b3b54dc9884dd83d2dcf54f4886cfe91aa0d09a3
Johnathan Van Why <jrvanwhy@betterbytes.org>
2026-08-28 17:07:43 -0700

    bus_target_cfg: Clarify new() and impl Address docs.

--- examples/bus_target_cfg.rs
+++ examples/bus_target_cfg.rs
@@ -15,14 +15,16 @@ use tock_registers::{register_map, Address, Bus, Read};
 /// A Bus for x86's port I/O. This only implements byte-sized reads to limit the size of this
 /// example.
 #[derive(Clone, Copy)]
-pub struct Port {
+pub struct PortIo {
     number: u16,
     _phantom: PhantomData<*mut ()>,
 }
 
-impl Port {
-    /// Returns a new Port with the given number.
-    // This is optional as well, but kept for this example because we use it in main().
+impl PortIo {
+    /// Returns a new PortIo with the given number.
+    // register_map allows the constructor to be #[cfg(target_arch = ...)] as well. However, this
+    // example calls PortIo::new from main() regardless of the target architecture, so we don't
+    // demonstrate that cfg in this example.
     pub fn new(number: u16) -> Self {
         Self {
             number,
@@ -31,12 +33,12 @@ impl Port {
     }
 }
 
-// `register_map!` emits offset tests that run on every architecture, including unit test
-// environments. Those test depend on the Bus trait, which has Address as a supertrait. Therefore,
-// we have to implement Address regardless of the target.
-impl Address for Port {
-    unsafe fn byte_add(self, offset: usize) -> Port {
-        Port {
+// `register_map!` emits offset tests that run regardless of what target the code is built for,
+// including unit test environments. Those test depend on the Bus trait, which has Address as a
+// supertrait. Therefore, we cannot put #[cfg(target_arch = ...)] on the Address implementation.
+impl Address for PortIo {
+    unsafe fn byte_add(self, offset: usize) -> PortIo {
+        PortIo {
             number: self.number + offset as u16,
             _phantom: PhantomData,
         }
@@ -44,26 +46,26 @@ impl Address for Port {
 }
 
 // Safety: Byte registers are 1 apart in I/O ports.
-unsafe impl Bus<u8> for Port {
+unsafe impl Bus<u8> for PortIo {
     const PADDED_SIZE: usize = 1;
 }
 
 #[cfg(target_arch = "x86")]
-impl tock_registers::BusRead<u8> for Port {
+impl tock_registers::BusRead<u8> for PortIo {
     unsafe fn read(self) -> u8 {
         0
     }
 }
 
 register_map! {
-    #[bus(Port)]
+    #[bus(PortIo)]
     registers {
         0 => status: u8 { Read },
     }
 }
 
 fn main() {
-    let _real = unsafe { registers::Real::new(Port::new(1)) };
+    let _real = unsafe { registers::Real::new(PortIo::new(1)) };
     // This won't compile on non-x86 architectures, because `Real` does not implement Interface.
     #[cfg(target_arch = "x86")]
     {
